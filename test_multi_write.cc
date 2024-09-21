be5d5e5c7e89a27175560cb33a77ba44cb659cc7
David Faitelson <davidf@afeka.ac.il>
2024-09-21 17:35:19 +0300

    testing a multiprogram for acquiring a cell without locks

---
+++ test_multi_write.cc
@@ -0,0 +1,56 @@
+#include <cassert>
+#include <algorithm>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+const int N = 100;
+
+int b[N];
+
+void work(int id) {
+
+	assert(id > 0);
+
+	chrono::milliseconds t(10 + (rand() % 2000));
+
+	this_thread::sleep_for(t);
+
+	int k = 0;
+
+	while (b[k] != id) {
+		while (b[k] != 0)
+			++k;
+		assert(k < N);
+		b[k] = id;
+	}
+}
+
+int main()
+{
+
+	thread t[N];
+
+	for(int i = 0; i < N; i++) {
+		t[i] = thread(work, i+1);
+	}
+
+	for(int i = 0; i < N ; i++) {
+		t[i].join();
+	}
+
+	for(int i = 0; i < N; i++) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+	sort(b, b+N);
+
+	for(int i = 0; i < N; i++) {
+
+		assert( b[i] == i+1 );
+	}
+
+	return 0;
+}
